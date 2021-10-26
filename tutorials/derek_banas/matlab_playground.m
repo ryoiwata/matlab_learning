ce6515111972594a8e9cc7b0ceb890e501162761
ryoiwata <ryoiwata@github.com>
2021-10-26 19:57:51 -0400

    first script that adds a vector up

--- tutorials/derek_banas/matlab_playground.m
+++ tutorials/derek_banas/matlab_playground.m
@@ -1,3 +1,8 @@
-if 120985329332/ 3 == 1
-    fprintf("woah")
+function sum_of_vector = get_sum_of_vector(input_array):
+    sum_of_vector = 0
+    length_of_array = numel(input_array)
+    for idx = 1:numel(length_of_array)
+        element = input_array(idx)
+        sum_of_vector = sum_of_vector + element
+    end
 end
